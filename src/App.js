33795c8b8a82a17f977a38a5fcbf76b1347a2a4f
daddyawesome <sablay296@gmail.com>
2020-01-12 00:15:38 +0800

    make a tweet componet on app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import Hello from './sayHello';
+import React, { useState } from 'react';
+import Tweet from './tweet';
 
 function App(){
-  
+  const [isRed,setRed] = useState(false);
+  const [count, setCount] = useState(0);
 
+  const [users,setUsers] = useState([
+    {name: "Daddy Awesome", message:"I am awesome"},
+    {name: "John", message:"I am Jon Snow"},
+    {name: "russel", message:"Russel by name russel by reputation"},
+  ]
+  );
+
+  const increment = () => {
+    setCount(count +1)
+    setRed(!isRed)
+  };
   return(
-    <div>
-      <h1>This is the App Component</h1>
-      <Hello />
+    <div className='app'>
+      <h1 className = {isRed ? 'red': ''}>Change my Color</h1>
+      <button onClick ={increment}>Increment</button>
+      <h1>{count}</h1>
+      {users.map(user =>
+        <Tweet name={user.name} message={user.message} />
+        )}
+      
+      
     </div>
   );
 }
